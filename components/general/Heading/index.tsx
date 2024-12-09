390057f9bbf626bc1cbbf5e6259cac3c5325dbbc
amrhe <amr.hassan.emam@gmail.com>
2024-12-10 00:50:36 +0200

    add instructors page

--- components/general/Heading/index.tsx
+++ components/general/Heading/index.tsx
@@ -2,11 +2,13 @@ import React from "react";
 
 export function H2({
   children,
+  classNames,
 }: Readonly<{
   children: React.ReactNode;
+  classNames?: string 
 }>) {
   return (
-    <h2 className="text-2xl lg:text-5xl font-protestRiot text-purple-dark">
+    <h2 className={`text-2xl lg:text-5xl font-protestRiot text-purple-dark ${classNames}`}>
       {children}
     </h2>
   );
